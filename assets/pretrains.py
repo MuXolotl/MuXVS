fa92bf2d2b7d8923e3316b7dca9dd2f12b0e617b
Artyom Bebroy <artyom.vasilenko.99@list.ru>
2026-09-23 16:55:57 +0500

    Update pretrains.py

--- assets/pretrains.py
+++ assets/pretrains.py
@@ -10,22 +10,23 @@ DEFAULT_PRETRAIN = "Default"
 
 # Набор -> {частота: (D-файл, G-файл)}; пути относительно pretrained/v2/<частота>/.
 PRETRAINS = {
+    # Основные претрейны
     "Default": {
         "32k": ("Default/f0D32k.pth", "Default/f0G32k.pth"),
         "40k": ("Default/f0D40k.pth", "Default/f0G40k.pth"),
         "48k": ("Default/f0D48k.pth", "Default/f0G48k.pth"),
     },
-    "Snowie v3.1": {
-        "32k": ("Snowie/D_SnowieV3.1_32k.pth", "Snowie/G_SnowieV3.1_32k.pth"),
-        "40k": ("Snowie/D_SnowieV3.1_40k.pth", "Snowie/G_SnowieV3.1_40k.pth"),
-        "48k": ("Snowie/D_SnowieV3.1_48k.pth", "Snowie/G_SnowieV3.1_48k.pth"),
+    "Snowie": {
+        "40k": ("Snowie/D_Snowie_40k.pth", "Snowie/G_Snowie_40k.pth"),
     },
     "Snowie v2": {
         "40k": ("Snowie/D_SnowieV2_40k.pth", "Snowie/G_SnowieV2_40k.pth"),
         "48k": ("Snowie/D_SnowieV2_48k.pth", "Snowie/G_SnowieV2_48k.pth"),
     },
-    "Snowie": {
-        "40k": ("Snowie/D_Snowie_40k.pth", "Snowie/G_Snowie_40k.pth"),
+    "Snowie v3.1": {
+        "32k": ("Snowie/D_SnowieV3.1_32k.pth", "Snowie/G_SnowieV3.1_32k.pth"),
+        "40k": ("Snowie/D_SnowieV3.1_40k.pth", "Snowie/G_SnowieV3.1_40k.pth"),
+        "48k": ("Snowie/D_SnowieV3.1_48k.pth", "Snowie/G_SnowieV3.1_48k.pth"),
     },
     "Snowie-X-Rin": {
         "40k": ("Snowie/D_Snowie-X-Rin_40k.pth", "Snowie/G_Snowie-X-Rin_40k.pth"),
@@ -40,87 +41,23 @@ PRETRAINS = {
         "40k": ("KLM/D_KLM43_X3_40k.pth", "KLM/G_KLM43_X3_40k.pth"),
         "48k": ("KLM/D_KLM43_X3_48k.pth", "KLM/G_KLM43_X3_48k.pth"),
     },
-    "KLM v4.3 x2": {
-        "32k": ("KLM/D_KLM43_x2_32k.pth", "KLM/G_KLM43_x2_32k.pth"),
+    "KLM v4.9": {
+        "32k": ("KLM/D_KLM_HFG_32k.pth", "KLM/G_KLM_HFG_32k.pth"),
+        "40k": ("KLM/D_KLM_HFG_40k.pth", "KLM/G_KLM_HFG_40k.pth"),
+        "48k": ("KLM/D_KLM_HFG_48k.pth", "KLM/G_KLM_HFG_48k.pth"),
     },
-    "KLM v4.3 x1": {
-        "32k": ("KLM/D_KLM43_x1_32k.pth", "KLM/G_KLM43_x1_32k.pth"),
-    },
-    "KLM v4.2 x10": {
-        "32k": ("KLM/D_KLM42_32k_x10.pth", "KLM/G_KLM42_32k_x10.pth"),
-    },
-    "KLM v4.2 fp32 T3": {
-        "40k": ("KLM/D_KLM42_fp32_T3_40k.pth", "KLM/G_KLM42_fp32_T3_40k.pth"),
-    },
-    "KLM v4.2 fp32 T2": {
-        "40k": ("KLM/D_KLM42_fp32_T2_40k.pth", "KLM/G_KLM42_fp32_T2_40k.pth"),
-    },
-    "KLM v4.2 fp32 T1": {
-        "40k": ("KLM/D_KLM42_fp32_T1_40k.pth", "KLM/G_KLM42_fp32_T1_40k.pth"),
-    },
-    "KLM v4.2 T4": {
-        "40k": ("KLM/D_KLM42_T4_40k.pth", "KLM/G_KLM42_T4_40k.pth"),
-    },
-    "KLM v4.2 T1": {
-        "32k": ("KLM/D_KLM42_T1_32k.pth", "KLM/G_KLM42_T1_32k.pth"),
-    },
-    "KLM v4.1 T2": {
-        "32k": ("KLM/D_KLM41_T2_32k.pth", "KLM/G_KLM41_T2_32k.pth"),
-    },
-    "KLM v4.1": {
-        "32k": ("KLM/D_KLM41_32k.pth", "KLM/G_KLM41_32k.pth"),
-        "48k": ("KLM/D_KLM41_48k.pth", "KLM/G_KLM41_48k.pth"),
-    },
-    "KLM v4.0": {
-        "32k": ("KLM/D_KLM40_32k.pth", "KLM/G_KLM40_32k.pth"),
-        "40k": ("KLM/D_KLM40_40k.pth", "KLM/G_KLM40_40k.pth"),
-        "48k": ("KLM/D_KLM40_48k.pth", "KLM/G_KLM40_48k.pth"),
-    },
-    "Ov2": {
-        "32k": ("Ov2/f0Ov2Super32kD.pth", "Ov2/f0Ov2Super32kG.pth"),
-        "40k": ("Ov2/f0Ov2Super40kD.pth", "Ov2/f0Ov2Super40kG.pth"),
-    },
-    "RIN E3": {
-        "40k": ("RIN_E/D_RIN_E3.pth", "RIN_E/G_RIN_E3.pth"),
-    },
+    # Остальные претрейны (в алфовитном порядке)
     "Anime": {
         "32k": ("Anime/f0D_AnimePreTrain.pth", "Anime/f0G_AnimePreTrain.pth"),
     },
-    "DMR v2": {
-        "32k": ("DMR/D_DMR-V2.pth", "DMR/G_DMR-V2.pth"),
+    "Aurora": {
+        "32k": ("Aurora/D_Aurora_v2.pth", "Aurora/G_Aurora_v2.pth"),
     },
     "DMR v1": {
         "32k": ("DMR/D_DMR-V1.pth", "DMR/G_DMR-V1.pth"),
     },
-    "DMR v0.8": {
-        "32k": ("DMR/D_DMR_V0-8.pth", "DMR/G_DMR_V0-8.pth"),
-    },
-    "DMR v0.5": {
-        "32k": ("DMR/D_dmrV0-5.pth", "DMR/G_dmrV0-5.pth"),
-    },
-    "IMA Robotic": {
-        "32k": ("IMA/D_IMA_Robotic.pth", "IMA/G_IMA_Robotic.pth"),
-    },
-    "ItaIla": {
-        "32k": ("ItaIla/ItaIla_32k_D.pth", "ItaIla/ItaIla_32k_G.pth"),
-    },
-    "Nanashi v1.5": {
-        "32k": ("Nanashi/D_nanashi_v1_5.pth", "Nanashi/G_nanashi_v1_5.pth"),
-    },
-    "Nanashi v1": {
-        "32k": ("Nanashi/D_nanashi_v1.pth", "Nanashi/G_nanashi_v1.pth"),
-    },
-    "Nezox": {
-        "32k": ("Nezox/Nezox_32k_D.pth", "Nezox/Nezox_32k_G.pth"),
-    },
-    "Rigel": {
-        "32k": ("Rigel/D_Rigel_32k.pth", "Rigel/G_Rigel_32k.pth"),
-    },
-    "VocalCoreTry3b": {
-        "48k": ("VocalCore/D_VocalCoreTry3b.pth", "VocalCore/G_VocalCoreTry3b.pth"),
-    },
-    "VocalCore": {
-        "48k": ("VocalCore/D_VocalCore.pth", "VocalCore/G_VocalCore.pth"),
+    "DMR v2": {
+        "32k": ("DMR/D_DMR-V2.pth", "DMR/G_DMR-V2.pth"),
     },
     "GuideVocalPretrain": {
         "48k": (
@@ -128,17 +65,48 @@ PRETRAINS = {
             "GuideVocalPretrain/G_GuideVocalPretrain.pth",
         ),
     },
-    "Singer 2": {
-        "32k": ("Singer/f0D_SingerPreTrain2.pth", "Singer/f0G_SingerPreTrain2.pth"),
+    "IMA Robotic": {
+        "32k": ("IMA/D_IMA_Robotic.pth", "IMA/G_IMA_Robotic.pth"),
+    },
+    "ItaIla": {
+        "32k": ("ItaIla/ItaIla_32k_D.pth", "ItaIla/ItaIla_32k_G.pth"),
+    },
+    "Nanashi v1": {
+        "32k": ("Nanashi/D_nanashi_v1.pth", "Nanashi/G_nanashi_v1.pth"),
+    },
+    "Nanashi v1.5": {
+        "32k": ("Nanashi/D_nanashi_v1_5.pth", "Nanashi/G_nanashi_v1_5.pth"),
+    },
+    "Nezox": {
+        "32k": ("Nezox/Nezox_32k_D.pth", "Nezox/Nezox_32k_G.pth"),
+    },
+    "Ov2": {
+        "32k": ("Ov2/f0Ov2Super32kD.pth", "Ov2/f0Ov2Super32kG.pth"),
+        "40k": ("Ov2/f0Ov2Super40kD.pth", "Ov2/f0Ov2Super40kG.pth"),
+    },
+    "Rigel": {
+        "32k": ("Rigel/D_Rigel_32k.pth", "Rigel/G_Rigel_32k.pth"),
+    },
+    "RIN E3": {
+        "40k": ("RIN_E/D_RIN_E3.pth", "RIN_E/G_RIN_E3.pth"),
     },
     "Singer": {
         "32k": ("Singer/f0D_SingerPreTrain.pth", "Singer/f0G_SingerPreTrain.pth"),
+    },
+    "Singer 2": {
+        "32k": ("Singer/f0D_SingerPreTrain2.pth", "Singer/f0G_SingerPreTrain2.pth"),
     },
     "UK-A": {
         "32k": ("UK/UKA-Pretrain-D.pth", "UK/UKA-Pretrain-G.pth"),
     },
     "UK-R": {
         "32k": ("UK/UKR-Pretrain-D.pth", "UK/UKR-Pretrain-G.pth"),
+    },
+    "VocalCore": {
+        "48k": ("VocalCore/D_VocalCore.pth", "VocalCore/G_VocalCore.pth"),
+    },
+    "VocalCoreTry3b": {
+        "48k": ("VocalCore/D_VocalCoreTry3b.pth", "VocalCore/G_VocalCoreTry3b.pth"),
     },
 }
 
